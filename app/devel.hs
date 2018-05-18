49a588487b0bf237fae84467a67dcd96d7aee6d9
Jhordy Salinas <jhordy.salinas@mail.escuelaing.edu.co>
2018-05-18 18:00:11 -0500

    Inicial

---
+++ app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "paniniPfis" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
---
+++ src/Handler/Panini.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes, TypeFamilies, GeneralizedNewtypeDeriving, TemplateHaskell, OverloadedStrings, FlexibleContexts, GADTs  #-}
+module Handler.Panini where
+
+import Import
+import Database.Persist.Sql
+
+getUserLaminaR :: Handler Html
+getUserLaminaR = do
+
+    allComments <- runDB $ rawSql s []
+    defaultLayout $ do
+        setTitle "Welcome To Yesod!"
+        $(widgetFile "paninipage")
+            where s = "SELECT ?? FROM (SELECT lamina as id,lamina as lamina, CAST(SUM(cantidad) as int8) as cantidad FROM user_lamina GROUP BY lamina) table_lamina ORDER BY lamina"
+
+{-
+getUserLaminaR :: Handler Html
+getUserLaminaR = do
+    allComments <- runDB $ selectList [] []
+    defaultLayout $ do
+        setTitle "Welcome To Yesod!"
+        $(widgetFile "paninipage")
+
+
+
+$if null allComments
+    <p> no hay
+$else
+    <table>
+        <thead>
+            <tr>
+                <th>Usuario</th>
+                <th>Lamina</th> 
+                <th>Cantidad</th>
+        <tbody>
+            $forall Entity userLaminaid userLamina <- allComments
+                <tr>
+                    <td>#{show (userLaminaLamina userLamina)} 
+                    <td>#{userLaminaLamina userLamina}
+                    <td>#{userLaminaCantidad userLamina}
+-}
