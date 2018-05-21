e150319750d99b85df33bef916a8dcc09ac75384
Carlos Andres Sanchez Venegas <carlos.sanchez-v@mail.escuelaing.edu.co>
2018-05-21 18:28:17 -0500

    agregando perfil

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -6,10 +6,34 @@
 module Handler.Profile where
 
 import Import
+import Database.Persist.Sql
+import Yesod.Form.Bootstrap3 (BootstrapFormLayout(..),renderBootstrap3, bfs)
+import Yesod.Form.Nic (nicHtmlField)
+import Yesod
+
+
+formNewLamina :: UserId -> Form UserLamina
+formNewLamina userId = renderBootstrap3 BootstrapBasicForm $ UserLamina
+          <$> pure  userId
+          <*> areq intField "Lamina: " Nothing
+          <*> areq intField "Cantidad: " Nothing
+
 
 getProfileR :: Handler Html
 getProfileR = do
     (_, user) <- requireAuthPair
+    --idUserArray <- runDB $ (getUserId (userIdent user))
+    userLaminas <- runDB $ (getUserLaminasQuery (userIdent user))
+    --(newLaminaWidget, enctype) <- generateFormPost (formNewLamina (head idUserArray))
     defaultLayout $ do
         setTitle . toHtml $ userIdent user <> "'s User page"
         $(widgetFile "profile")
+
+getUserId :: MonadIO m => Text ->  ReaderT SqlBackend m [UserId]
+getUserId user = rawSql s []
+    where s = "select id from public.user where ident = '"++ user ++"'"
+getUserLaminasQuery :: MonadIO m => Text ->  ReaderT SqlBackend m [(Single Int, Single Int)]
+getUserLaminasQuery user = rawSql s []
+    where s = "select lamina,cantidad \
+            \from user_lamina where user_lamina.user = (select id from public.user where ident = '"++ user ++"') "
+             
